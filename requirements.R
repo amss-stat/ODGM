bcc7194a9945209115cb3900eecaef9c1db0e870
amss-stat <amss_stat@163.com>
2026-06-23 03:00:16 +0800

    Update requirements.R

--- requirements.R
+++ requirements.R
@@ -1,4 +1,4 @@
-# R packages required by figures/Figure7_TSM_positions.R
+# R packages required by figures/Figure7.R
 #
 # Run in R:
 # source("requirements.R")
@@ -7,11 +7,3 @@ required_packages <- c(
   "ggplot2",
   "patchwork"
 )
-
-missing_packages <- required_packages[
-  !vapply(required_packages, requireNamespace, logical(1), quietly = TRUE)
-]
-
-if (length(missing_packages) > 0) {
-  install.packages(missing_packages, repos = "https://cloud.r-project.org")
-}
